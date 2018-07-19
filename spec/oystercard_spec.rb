c1c72e99c53ec0ecb7af8b908633d19781e7fafd
dani <daniboo83@gmail.com>
2018-07-19 13:50:49 +0100

    Updated Gemfile for rubocop

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,31 +5,31 @@ describe Oystercard do
 
   context 'money on card' do
     describe '#balance' do
-      # card responds to balance method
-      it 'responds to balance' do
+      
+      it 'responds to balance' do # card responds to balance method
         expect(subject).to respond_to(:balance)
       end
-      # card has balance of zero
-      it 'has a balance of zero' do
+      
+      it 'has a balance of zero' do # card has balance of zero
         expect(subject.balance).to eq(0)
       end
-      # balance is an integer
-      it 'checks that balance is an integer' do
+     
+      it 'checks that balance is an integer' do  # balance is an integer
         expect(subject.balance).to be_a Integer
       end
     end
 
     describe '#top_up' do
-      # card responds to top_up method
-      it 'responds to top_up with 1 argument' do
+      
+      it 'responds to top_up with 1 argument' do # card responds to top_up method
         expect(subject).to respond_to(:top_up).with(1).argument
       end
-      # card can be topped up
-      it 'adds top-up amount to balance' do
+      
+      it 'adds top-up amount to balance' do # card can be topped up
         expect(subject.top_up(1)).to eq 1
       end
-      # card doesn't exceed max limit
-      it 'raises error when balance exceeds max_limit' do
+      
+      it 'raises error when balance exceeds max_limit' do # card doesn't exceed max limit
         max_balance = Oystercard::MAX_LIMIT
         subject.top_up(max_balance)
         expect { subject.top_up 1 }.to raise_error "Maximum balance of #{Oystercard::MAX_LIMIT} pounds exceeded"
@@ -37,51 +37,52 @@ describe Oystercard do
     end
 
     describe '#deduct(amount)' do
-      # card responds to deduct method
-      it 'responds to deduct' do
+      
+      it 'responds to deduct' do # card responds to deduct method
         expect(subject).to respond_to(:deduct).with(1).argument
       end 
-      # money gets deducted
-      it 'deducts money' do
+      
+      it 'deducts money' do # money gets deducted
         expect(subject.deduct(1)).to eq -1
       end
     end
-  end
+  end # end of context 'money on card'
 
-  context 'in journey' do
-    before(:each) { subject.balance = 10 }
-      describe '#touch_in' do     
-      # responds to tapping in
-      it { is_expected.to respond_to(:touch_in).with(1).argument }
-      # touches in means in_journey
-      it 'touches in to be in_journey' do
+  context 'travel' do
+  before(:each) { subject.balance = 10 }
+
+    describe '#touch_in' do 
+      
+      it { is_expected.to respond_to(:touch_in).with(1).argument } # responds to tapping in
+      
+      it 'touches in to be in_journey' do # touches in means in_journey
         subject.touch_in(station)
         expect(subject.in_journey?).to eq true
       end
     end
 
     describe '#entry_station' do
+
       it 'saves the entry station' do
-        # puts subject.balance
         subject.touch_in(station)
         expect(subject.entry_station).to eq station
       end
     end
 
     describe '#in_journey?' do
-      # responds to in_journey?
-      it { is_expected.to respond_to(:in_journey?) }
-      # initially not in journey
-      it 'starts out not in journey' do
+      
+      it { is_expected.to respond_to(:in_journey?) } # responds to in_journey?
+      
+      it 'starts out not in journey' do # initially not in journey
         expect(subject.in_journey?).to eq false
       end
     end
 
     describe '#touch_out' do
-      # responds to tapping out
-      it { is_expected.to respond_to(:touch_out) }
-      # touches out means not in_journey
-      it 'touches out to not be in_journey' do
+      
+      it { is_expected.to respond_to(:touch_out) } # responds to tapping out
+
+      it 'touches out to not be in_journey' do # touches out means not in_journey
         subject.touch_out
         expect(subject.in_journey?).to eq false
       end 
@@ -89,12 +90,12 @@ describe Oystercard do
         expect { subject.touch_out }.to change { subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
       end
     end
-  end
+  end # end of context 'travel'
 
   describe '#minimum_balance' do
     before(:each) { subject.balance < 1 }
+
     it 'won\'t touch in if below minimum balance' do
-      # subject.balance < Oystercard::MINIMUM_CHARGE
       expect { subject.touch_in(station) }.to raise_error "You need at least #{Oystercard::MINIMUM_CHARGE} pound to tap in. Please top up."
     end
   end 
